1b7bba8eb324fc0a4d642da85191bcc537305724
ames <ampi@ximedes.com>
2024-11-12 13:09:16 +0100

    fixed in de code that return StringIndexOutOfBoundsException

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -12,13 +12,12 @@ public class GHappyTest {
         System.out.print("HAPPY!\n");
     }
 
-// This case gives an error StringIndexOutOfBoundsException : Index -1 out of bounds for length 3
-//    @Test
-//    public void test02(){
-//        GHappy a = new GHappy();
-//        Assertions.assertFalse(a.gHappy("gxx"));
-//        System.out.print("NO HAPPY!\n");
-//    }
+    @Test
+    public void test02(){
+        GHappy a = new GHappy();
+        Assertions.assertFalse(a.gHappy("gxxx"));
+        System.out.print("NO HAPPY!\n");
+    }
 
     @Test
     public void test03(){
