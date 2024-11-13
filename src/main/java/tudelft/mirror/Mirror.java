13c3f35cafff446aa618b56e714f211302878fc6
ames <ampi@ximedes.com>
2024-11-13 11:24:02 +0100

    fixed bug and add tests for Mirror

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -3,19 +3,18 @@ package tudelft.mirror;
 public class Mirror {
 
     public String mirrorEnds(String string) {
-        String mirror = "";
+        StringBuilder mirror = new StringBuilder();
 
         int begin = 0;
         int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
-            if (string.charAt(begin) == string.charAt(end)) {
-                mirror += String.valueOf(string.charAt(end));
-            }
-            else {
-                break;
-            }
+
+        // Traverse from both ends to find mirrored characters
+        while (begin < string.length() && end >= 0 && string.charAt(begin) == string.charAt(end)) {
+            mirror.append(string.charAt(begin));
+            begin++;
+            end--;
         }
 
-        return begin == end ? string : mirror;
+        return mirror.toString();
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,63 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void testMirrorEndsFullMatch() {
+        Mirror mirror = new Mirror();
+        String input = "abccba";
+        String expected = "abccba";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
+
+    @Test
+    public void testMirrorEndsPartialMatch() {
+        Mirror mirror = new Mirror();
+        String input = "abca";
+        String expected = "a";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
+
+    @Test
+    public void testMirrorEndsNoMatch() {
+        Mirror mirror = new Mirror();
+        String input = "abc";
+        String expected = "";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
+
+    @Test
+    public void testMirrorEndsSingleCharacter() {
+        Mirror mirror = new Mirror();
+        String input = "a";
+        String expected = "a";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
+
+    @Test
+    public void testMirrorEndsEmptyString() {
+        Mirror mirror = new Mirror();
+        String input = "";
+        String expected = "";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
+
+    @Test
+    public void testMirrorEndsOddLengthMatch() {
+        Mirror mirror = new Mirror();
+        String input = "racecar";
+        String expected = "racecar";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
+
+    @Test
+    public void testMirrorEndsEvenLengthPartialMatch() {
+        Mirror mirror = new Mirror();
+        String input = "abcddcbaef";
+        String expected = "abcddcba";
+        Assertions.assertEquals(expected, mirror.mirrorEnds(input));
+    }
 }
